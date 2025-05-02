eeeffeb8b0dd13fe6c95d44c824ae17f1b529f48
chia-sx <2302537b@student.tp.edu.sg>
2025-05-02 14:02:16 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+function db_connect() {
+    $servername = "localhost";
+    $username = "root";
+    $password = "";     
+    $dbname = ""; 
+
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    return $conn;
+}
+?>
