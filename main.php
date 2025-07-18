7f113b26747fe5ab641f7299b2cecde43cfbc9a5
w3ihhh <2302536D@student.tp.edu.sg>
2025-07-18 13:05:55 +0800

    Update main.php

--- main.php
+++ main.php
@@ -176,7 +176,7 @@ if (isset($_SESSION['user_id'])) {
                 <ul class="dropdown-menu" aria-labelledby="aboutDropdown">
                     <li><a class="dropdown-item" href="mcq_quiz.php">Mini Quiz</a></li>
                     <li><a class="dropdown-item" href="basic_python.php">Python Quiz</a></li>
-                    <li><a class="dropdown-item" href="#">Capture The Flag</a></li>
+                    <li><a class="dropdown-item" href="ctf_challenge">Capture The Flag</a></li>
                     <li><a class="dropdown-item" href="forensics_challenge.php">Forensics Challenge</a></li>
                     
                 </ul>
