e28238acc64c2245e2199275f175f7e7c2826996
w3ihhh <2302536D@student.tp.edu.sg>
2025-07-24 00:56:31 +0800

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -18,6 +18,7 @@ if (isset($_SESSION['user_id'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="mstyles.css">
     <title>Document</title>
  </head>
